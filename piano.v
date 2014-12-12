c2e065004ea598780bb483a83178d6174b0e8952
Ben Kahle <ben@kahle.org>
2014-12-12 17:25:12 -0500

    a likely failed attempt to do timing stuff.

--- piano.v
+++ piano.v
@@ -23,8 +23,11 @@ module piano(clk, sw, btn, n1, n2, n3, n4, n5, n6, n7, n8);
 input clk;
 input[7:0] sw;
 input[3:0] btn;
-output wire n1, n2, n3, n4, n5, n6, n7, n8;
-wire note1, note2, note3, note4, note5, note6, note7, note8;
+output reg n1, n2, n3, n4, n5, n6, n7, n8;
+reg[7:0] octaveNotes[7:0] = {8'd1, 8'd2, 8'd3, 8'd4, 8'd5, 8'd6, 8'd7, 8'd8};
+reg[3:0] octaveCounter;
+reg octavePosition;
+wire note1, note2, note3, note4, note5, note6, note7, note8, fullNote, halfNote, quarterNote;
 note #(25000000/262/2) c4(clk, note1);
 note #(25000000/294/2) d(clk, note2);
 note #(25000000/330/2) e(clk, note3);
@@ -33,14 +36,49 @@ note #(25000000/392/2) g(clk, note5);
 note #(25000000/440/2) a(clk, note6);
 note #(25000000/494/2) b(clk, note7);
 note #(25000000/523/2) c5(clk, note8);
-assign n1 = note1 & sw[7];
-assign n2 = note2 & sw[6];
-assign n3 = note3 & sw[5];
-assign n4 = note4 & sw[4];
-assign n5 = note5 & sw[3];
-assign n6 = note6 & sw[2];
-assign n7 = note7 & sw[1];
-assign n8 = note8 & sw[0];
+note #(25000000/12500000/2) fN(clk,fullNote);
+note #(25000000/12500000/2) hN(clk,fullNote);
+note #(25000000/6250000/2) qN(clk,fullNote);
+always @(posedge clk) begin
+  if(!btn) begin
+    n1 = note1 & sw[7];
+    n2 = note2 & sw[6];
+    n3 = note3 & sw[5];
+    n4 = note4 & sw[4];
+    n5 = note5 & sw[3];
+    n6 = note6 & sw[2];
+    n7 = note7 & sw[1];
+    n8 = note8 & sw[0];
+  end
+  else begin
+    if (btn[0]) begin // Play octave
+      if(fullNote && !octavePosition) begin //Increment octave counter on fullNote timing
+        octaveCounter = octaveCounter + 1;
+        octavePosition = 1;
+      end
+      if(!fullNote && octavePosition) begin
+        octavePosition = 0;
+      end
+      n1 = note1 & sw[7];
+      n2 = note2 & sw[6];
+      n3 = note3 & sw[5];
+      n4 = note4 & sw[4];
+      n5 = note5 & sw[3];
+      n6 = note6 & sw[2];
+      n7 = note7 & sw[1];
+      n8 = note8 & sw[0];
+    end
+    // if (btn[1]) begin
+      
+    // end
+    // if (btn[2]) begin
+      
+    // end
+    // if (btn[3]) begin
+      
+    // end
+  end
+end
 endmodule
 
 module testpiano;
